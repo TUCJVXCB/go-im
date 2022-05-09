2f419aec5ea35277ff23272b0fa9dd3fcc9457c8
tanyu <838806482@qq.com>
2022-05-09 17:30:12 +0800

    5

--- user.go
+++ user.go
@@ -25,29 +25,40 @@ func NewUser(conn net.Conn, server *Server) *User {
 	return u
 }
 
-func (user *User) Online() {
-	user.server.lock.Lock()
-	user.server.userMap[user.name] = user
-	user.server.lock.Unlock()
+func (u *User) Online() {
+	u.server.lock.Lock()
+	u.server.userMap[u.name] = u
+	u.server.lock.Unlock()
 
-	user.server.broadCast(user, "已上线")
+	u.server.broadCast(u, "已上线")
 }
 
-func (user *User) offline() {
-	user.server.lock.Lock()
-	delete(user.server.userMap, user.name)
-	user.server.lock.Unlock()
+func (u *User) offline() {
+	u.server.lock.Lock()
+	delete(u.server.userMap, u.name)
+	u.server.lock.Unlock()
 
-	user.server.broadCast(user, "已退出群聊")
+	u.server.broadCast(u, "已退出群聊")
 }
 
-func (user *User) DoMessage(msg string) {
-	user.server.broadCast(user, msg)
+func (u *User) sendMessage(msg string) {
+	u.conn.Write([]byte(msg + "\n"))
 }
 
-func (user *User) receiveMsg() {
+func (u *User) DoMessage(msg string) {
+	if "who" == msg {
+		for _, user := range u.server.userMap {
+			onlineMsg := "[" + user.addr + "]" + user.name + ":在线...\n"
+			u.sendMessage(onlineMsg)
+		}
+	} else {
+		u.server.broadCast(u, msg)
+	}
+}
+
+func (u *User) receiveMsg() {
 	for {
-		msg := <-user.C
-		user.conn.Write([]byte(msg + "\n"))
+		msg := <-u.C
+		u.conn.Write([]byte(msg + "\n"))
 	}
 }
